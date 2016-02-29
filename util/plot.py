231ae5e7f4ff45555119e4d31a17f86f950716f6
Graham Smith <grahamas@gmail.com>
2016-02-29 17:08:37 -0600

    Added MovieStream?

--- util/plot.py
+++ util/plot.py
@@ -1,15 +1,15 @@
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
+import numpy as np
 
 def luminance_periodograms(l_movies, l_names, smooth=None):
 	fig = plt.figure()
 	for mov in l_movies:
 		f, Pxx = mov.luminance_spectrum(smooth)
-		plt.semilogy(f, Pxx)
-		del mov.movie
+		plt.semilogy(np.log(f), Pxx)
 	f = f[1:]
-	plt.semilogy(f, 1/f)
-	plt.semilogy(f, 1/(f**2))
+	plt.semilogy(np.log(f), 1/f)
+	plt.semilogy(np.log(f), 1/(f**2))
 	l_names += ['power', 'power_sq']
 	plt.legend(l_names)
 	plt.xlabel('Frequency (Hz)')
