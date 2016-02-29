5f94fc964d0d36e609e17e4256726475d821ff7c
Graham A Smith <grahamas@gmail.com>
2016-02-29 17:10:47 -0600

    I have a bad feeling about this...

--- util/video.py
+++ util/video.py
@@ -5,6 +5,7 @@ def play_video_cap(vc, window_name='frame', fps=60, processing=[]):
     frame_len = int(1000.0 / fps)
     while (vc.isOpened()):
         ret, frame = vc.read()
+        print 'Frame: ',frame
         processed = reduce(lambda x,f: f(x), processing, frame)
         cv2.imshow(window_name, processed)
         if cv2.waitKey(frame_len) & 0xFF == ord('q'):
@@ -21,6 +22,7 @@ def binarize_median(frame):
     ret,th = cv2.threshold(frame,median,255,cv2.THRESH_BINARY)
     return th
 def binarize_otsu(frame):
+    frame = np.array(frame, np.uint8)
     ret,th = cv2.threshold(frame,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     return th
 def binarize_gotsu(frame):
@@ -33,3 +35,7 @@ def RGB_to_grayscale(frame):
 def grayscale_to_RGB(frame):
     return cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
 
+def binary_bytearray(frame):
+    b_frame = frame > 0
+    b_frame = np.packbits(b_frame)
+    return b_frame
